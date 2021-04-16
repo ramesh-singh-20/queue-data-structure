c9c5a69376ec8a388d6f0283a76519ecafd1ffad
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-04-15 22:38:11 -0400

    Implemented Queue data structure with array.

--- src/main/java/com/alphacoder/datastructure/QueueAsArrayImplementation.java
+++ src/main/java/com/alphacoder/datastructure/QueueAsArrayImplementation.java
@@ -4,16 +4,18 @@ public class QueueAsArrayImplementation {
     private static final int INIT_CAPACITY= 10;
     private static int CURRENT_CAPACITY;
     private int[] arr;
+    private int rear;
     private int head;
 
     public QueueAsArrayImplementation(){
         this.arr= new int[INIT_CAPACITY];
         CURRENT_CAPACITY= INIT_CAPACITY;
+        this.rear= -1;
         this.head= -1;
     }
 
     public boolean isEmpty(){
-        return (this.head==-1);
+        return (this.rear== -1 || this.head> this.rear);
     }
 
     public int head(){
@@ -21,5 +23,40 @@ public class QueueAsArrayImplementation {
             throw new UnsupportedOperationException("Queue is empty.");
         }
         return this.arr[head];
+    }
+
+    public void enQueue(int element){
+        this.head=0;
+        this.rear+=1;
+        if(this.rear==this.arr.length){
+            doubleCurrentCapacity();
+        }
+        arr[rear]= element;
+    }
+
+    public int deQueue(){
+        if(this.isEmpty()){
+            throw new UnsupportedOperationException("Stack is empty. Cannot perform Dequeue operation.");
+        }
+        this.head++;
+        return this.arr[head-1];
+
+    }
+
+
+
+    /*
+    Utility method to double the queue size once the current queue limit exhausts.
+     */
+    private void doubleCurrentCapacity(){
+        CURRENT_CAPACITY*=2;
+        int[] tempArr= new int[CURRENT_CAPACITY];
+
+        int i=0;
+        for(int element: this.arr){
+            tempArr[i]= element;
+            i++;
+        }
+        this.arr= tempArr;
     }
 }
--- src/test/unit/groovy/com/alphacoder/datastructure/QueueAsArrayImplementationTest.groovy
+++ src/test/unit/groovy/com/alphacoder/datastructure/QueueAsArrayImplementationTest.groovy
@@ -15,14 +15,141 @@ class QueueAsArrayImplementationTest extends Specification{
         result
     }
 
+    def 'Test QueueAsArrayImplementation | isEmpty() | Queue is not empty'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+        queue.enQueue(0)
+
+        when:
+        def result= queue.isEmpty()
+
+        then:
+        !result
+    }
+
     def 'Test QueueAsArrayImplementation | head() | Queue is empty'(){
         given:
-        def queue= new QueueAsArrayImplementation();
+        def queue= new QueueAsArrayImplementation()
+
+        when:
+        queue.head()
+
+        then:
+        thrown(UnsupportedOperationException)
+    }
+
+    def 'Test QueueAsArrayImplementation | head() | Queue is not empty'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+        queue.enQueue(0)
 
         when:
         def result= queue.head()
 
         then:
+        result== 0
+        and:
+        !queue.isEmpty()
+    }
+
+    def 'Test QueueAsArrayImplementation | deQueue() | Queue is empty'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+
+        when:
+        queue.deQueue()
+
+        then:
         thrown(UnsupportedOperationException)
+    }
+
+    def 'Test QueueAsArrayImplementation | deQueue() | Queue is not empty'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+        queue.enQueue(0)
+
+
+        when:
+        def result= queue.deQueue()
+
+        then:
+        result== 0
+        and:
+        queue.isEmpty()
+    }
+
+    def 'Test QueueAsArrayImplementation | deQueue() | Deleting multiple elements'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+        queue.enQueue(0)
+        queue.enQueue(1)
+        queue.enQueue(2)
+        queue.deQueue()
+
+        when:
+        def result= queue.deQueue()
+
+        then:
+        result== 1
+        queue.head()==2
+        !queue.isEmpty()
+    }
+
+    def 'Test QueueAsArrayImplementation | deQueue() | Deleting all elements'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+        queue.enQueue(0)
+        queue.enQueue(1)
+        queue.enQueue(2)
+        queue.deQueue()
+        queue.deQueue()
+
+        when:
+        def result= queue.deQueue()
+
+        then:
+        result== 2
+        queue.isEmpty()
+    }
+
+    def 'Test QueueAsArrayImplementation | enQueue() | inserting multiple elements'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+
+        when:
+        queue.enQueue(0)
+        queue.enQueue(1)
+        queue.enQueue(2)
+
+        then:
+        !queue.isEmpty()
+
+        and:
+        queue.head()==0
+    }
+
+    def 'Test QueueAsArrayImplementation | enQueue() | inserting elements greater than initial capacity'(){
+        given:
+        def queue= new QueueAsArrayImplementation()
+
+        when:
+        queue.enQueue(0)
+        queue.enQueue(1)
+        queue.enQueue(2)
+        queue.enQueue(3)
+        queue.enQueue(4)
+        queue.enQueue(5)
+        queue.enQueue(6)
+        queue.enQueue(7)
+        queue.enQueue(8)
+        queue.enQueue(9)
+        queue.enQueue(10)
+        queue.enQueue(11)
+
+        then:
+        !queue.isEmpty()
+
+        and:
+        queue.head()==0
     }
 }
